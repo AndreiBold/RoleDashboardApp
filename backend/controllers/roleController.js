a45f07674ae0871309a87ee8e95cc23cfa541643
Lenovo <andreibold254@gmail.com>
2023-05-14 22:51:55 +0300

    Rest api logic for roles

--- backend/controllers/roleController.js
+++ backend/controllers/roleController.js
@@ -1,17 +1,26 @@
 const asyncHandler = require('express-async-handler')
 
+const Role = require('../models/roleModel')
+
 // @desc   GET roles
 // @route  GET /api/roles
 // @access Private
 const getRoles = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get roles" });
+  const roles = await Role.find()
+  res.status(200).json(roles);
 })
 
 // @desc   GET role
 // @route  GET /api/role/:id
 // @access Private
 const getRole = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Get role ${req.params.id}` });
+  const role = await Role.findById(req.params.id)
+
+  if(!role) {
+    res.status(400)
+    throw new Error('Role not found')
+  }
+  res.status(200).json(role);
 })
 
 // @desc   CREATE role
@@ -22,21 +31,39 @@ const createRole = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please provide the role name");
   }
-  res.status(200).json({ message: "Create role" });
+
+  const role = await Role.create({ name: req.body.name})
+  res.status(200).json(role);
 })
 
 // @desc   UPDATE role
 // @route  PUT /api/roles/:id
 // @access Private
 const updateRole = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update role ${req.params.id}` });
+  const role = await Role.findById(req.params.id)
+
+  if(!role) {
+    res.status(400)
+    throw new Error('Role not found')
+  }
+
+  const updatedRole = await Role.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  res.status(200).json(updatedRole);
 })
 
 // @desc   DELETE role
 // @route  DELETE /api/roles/:id
 // @access Private
 const deleteRole = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete role ${req.params.id}` });
+  const role = await Role.findById(req.params.id)
+
+  if(!role) {
+    res.status(400)
+    throw new Error('Role not found')
+  }
+
+  await role.deleteOne()
+  res.status(200).json({ id: req.params.id });
 })
 
 module.exports = {
---
+++ backend/models/roleModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const roleSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a role name']
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Role', roleSchema)
